22c7c0e0621d0d50a5b60c6fa0484f55d05a0364
Ego Lydia <94656569+EgoLydia@users.noreply.github.com>
2022-07-20 09:39:48 +0100

    displayed options

--- src/components/OptionsComponent.vue
+++ src/components/OptionsComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="option">
-    <input type="radio" />
-    <label for="">{{ value }}</label>
+  <div :style="{ background: background }" class="option">
+    <input :id="'value' + index" :name="question" :value="value" type="radio" />
+    <label :for="'value' + index">{{ value }}</label>
   </div>
 </template>
 
@@ -11,12 +11,32 @@ export default {
     value: {
       default: "Batman",
     },
+    background: {
+      default: "#6767ff65",
+      type: String,
+    },
+    index: {
+      default: 0,
+      type: Number,
+    },
+    question: {
+      type: String,
+    },
   },
 };
 </script>
 
 <style lang="scss">
 .option {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  cursor: pointer;
   display: flex;
+  margin-bottom: 10px;
+  padding: 10px;
+  p {
+    margin-top: 17px;
+    margin-left: 10px;
+  }
 }
 </style>
